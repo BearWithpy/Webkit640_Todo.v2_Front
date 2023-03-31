470bcee6a991cfd8f6900cba979067d4d5a1ae98
Junsu Park <pksjmh5295@gmail.com>
2023-03-31 13:36:21 +0900

    10. func - React Event Handler: 여러 번 추가되는 에러 존재

---
+++ src/AddTodo.js
@@ -0,0 +1,51 @@
+import { Button, Grid, Paper, TextField } from "@mui/material"
+import React, { useState } from "react"
+
+const AddTodo = ({ add, items, setItems }) => {
+    const [item, setItem] = useState({ title: "" })
+
+    const onInputChange = (e) => {
+        item.title = e.target.value
+        setItem({ title: item.title })
+    }
+    const onButtonClick = (e) => {
+        add(item)
+        setItems([...items, item])
+        setItem({ title: "" })
+    }
+    const enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            onButtonClick()
+        }
+    }
+
+    return (
+        <div>
+            <Paper style={{ margin: 16, padding: 16 }}>
+                <Grid container>
+                    <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                        <TextField
+                            placeholder="Add Todo here :)"
+                            fullWidth
+                            onChange={onInputChange}
+                            value={item.title}
+                            onKeyDown={enterKeyEventHandler}
+                        />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button
+                            fullWidth
+                            color="secondary"
+                            variant="outlined"
+                            onClick={onButtonClick}
+                        >
+                            Save
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+    )
+}
+
+export default AddTodo
--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import "./App.css"
 
 import Todo from "./Todo"
-import { List, Paper } from "@mui/material"
+import { Container, List, Paper } from "@mui/material"
 
-import React from "react"
-import Hello from "./Hello"
+import React, { useState } from "react"
+import AddTodo from "./AddTodo"
 
 const App = () => {
-    const items = [
+    const [items, setItems] = useState([
         { id: "ID-0", title: "Todo 1", done: false },
         { id: "ID-1", title: "Todo 2", done: true },
         { id: "ID-2", title: "Todo 3", done: false },
         { id: "ID-3", title: "Todo 4", done: false },
         { id: "ID-4", title: "Todo 5", done: false },
-    ]
+    ])
+
+    const add = (i) => {
+        i.id = "ID-" + items.length
+        i.done = false
+        items.push(i)
+    }
 
     const todoItems = items.length > 0 && (
         <Paper style={{ margin: 16 }}>
@@ -27,8 +33,11 @@ const App = () => {
 
     return (
         <div className="App">
-            <Hello />
-            {todoItems}
+            {/* <Hello /> */}
+            <Container maxWidth="md">
+                <AddTodo add={add} items={items} setItems={setItems} />
+                <div className="todoList">{todoItems}</div>
+            </Container>
         </div>
     )
 }
