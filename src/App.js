c5988f2cab3a63450b78fbf2f189303b995cee10
Junsu Park <pksjmh5295@gmail.com>
2023-03-31 10:44:12 +0900

    functional props, state

--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ import React from "react"
 import Hello from "./Hello"
 
 const App = () => {
-    // const todoItems = this.state.items.length > 0 && (
-    //     <Paper style={{ margin: 16 }}>
-    //         <List>
-    //             {this.state.items.map((item, idx) => (
-    //                 <Todo item={item} key={item.id} />
-    //             ))}
-    //         </List>
-    //     </Paper>
-    // )
+    const item = { id: 0, title: "Todo 1", done: true }
     return (
         <div className="App">
             <Hello />
+            <Todo item={item} />
         </div>
     )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,32 +1,24 @@
 import { Checkbox, InputBase, ListItem, ListItemText } from "@mui/material"
 
-import React, { Component } from "react"
+import React from "react"
 
-class Todo extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            item: props.item,
-        }
-    }
-    render() {
-        return (
-            <ListItem>
-                <Checkbox checked={this.state.item.done} />
-                <ListItemText>
-                    <InputBase
-                        inputProps={{ "aria-label": "naked" }}
-                        type="text"
-                        id={String(this.state.item.id)}
-                        name={String(this.state.item.id)}
-                        value={this.state.item.title}
-                        multiline={true}
-                        fullWidth={true}
-                    />
-                </ListItemText>
-            </ListItem>
-        )
-    }
+const Todo = (props) => {
+    return (
+        <ListItem>
+            <Checkbox checked={props.item.done} />
+            <ListItemText>
+                <InputBase
+                    inputProps={{ "aria-label": "naked" }}
+                    type="text"
+                    id={String(props.item.id)}
+                    name={String(props.item.id)}
+                    value={props.item.title}
+                    multiline={true}
+                    fullWidth={true}
+                />
+            </ListItemText>
+        </ListItem>
+    )
 }
 
 export default Todo
