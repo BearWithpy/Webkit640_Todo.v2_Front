dfdf885b7787b91acb71cc91dc7f9a2697fa6895
Junsu Park <pksjmh5295@gmail.com>
2023-04-06 14:59:17 +0900

    REFACTOR

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -20,6 +20,7 @@ const AddTodo = ({ add }) => {
             onButtonClick()
         }
     }
+
     return (
         <Paper style={{ margin: 16, padding: 16 }}>
             <Grid container>
@@ -45,50 +46,6 @@ const AddTodo = ({ add }) => {
             </Grid>
         </Paper>
     )
-
-    // const [item, setItem] = useState({ title: "" })
-
-    // const onInputChange = (e) => {
-    //     item.title = e.target.value
-    //     setItem({ title: item.title })
-    // }
-    // const onButtonClick = (e) => {
-    //     add(item)
-    //     setItem({ title: "" })
-    // }
-    // const enterKeyEventHandler = (e) => {
-    //     if (e.key === "Enter") {
-    //         onButtonClick()
-    //     }
-    // }
-
-    // return (
-    //     <div>
-    //         <Paper style={{ margin: 16, padding: 16 }}>
-    //             <Grid container>
-    //                 <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-    //                     <TextField
-    //                         placeholder="Add Todo here :)"
-    //                         fullWidth
-    //                         onChange={onInputChange}
-    //                         value={item.title}
-    //                         onKeyDown={enterKeyEventHandler}
-    //                     />
-    //                 </Grid>
-    //                 <Grid xs={1} md={1} item>
-    //                     <Button
-    //                         fullWidth
-    //                         color="secondary"
-    //                         variant="outlined"
-    //                         onClick={onButtonClick}
-    //                     >
-    //                         Save
-    //                     </Button>
-    //                 </Grid>
-    //             </Grid>
-    //         </Paper>
-    //     </div>
-    // )
 }
 
 export default AddTodo
